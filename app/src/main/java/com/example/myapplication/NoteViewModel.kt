752faa7eeec605aee18e380696b1c210ffe60c1f
meidian12 <ameidianhadi12@gmail.com>
2021-05-26 17:18:49 +0800

    RoomDB(MVVM,Coroutines,LiveData)

---
+++ app/src/main/java/com/example/myapplication/NoteViewModel.kt
@@ -0,0 +1,29 @@
+package com.example.myapplication
+
+import android.app.Application
+import androidx.lifecycle.AndroidViewModel
+import androidx.lifecycle.LiveData
+
+
+class NoteViewModel(application: Application) : AndroidViewModel(application) {
+
+        private var noteRepository = NoteRepository(application)
+        private var notes: LiveData<List<Note>>? = noteRepository.getNotes()
+
+        fun insertNote(note: Note) {
+            noteRepository.insert(note)
+        }
+
+        fun getNotes(): LiveData<List<Note>>? {
+            return notes
+        }
+
+        fun deleteNote(note: Note) {
+            noteRepository.delete(note)
+        }
+
+        fun updateNote(note: Note) {
+            noteRepository.update(note)
+        }
+
+    }
